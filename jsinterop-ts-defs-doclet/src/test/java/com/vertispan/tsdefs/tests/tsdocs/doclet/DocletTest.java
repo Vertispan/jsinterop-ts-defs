709aba60e55ed98a4a71aaf8162dd0e908111dd1
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2026-06-17 01:00:46 +0300

    Correctly emit the FQN for linked members in TsDocs
    fix #116

--- jsinterop-ts-defs-doclet/src/test/java/com/vertispan/tsdefs/tests/tsdocs/doclet/DocletTest.java
+++ jsinterop-ts-defs-doclet/src/test/java/com/vertispan/tsdefs/tests/tsdocs/doclet/DocletTest.java
@@ -82,4 +82,9 @@ public class DocletTest {
   public void testIssue106() throws IOException {
     testDocs("links.issue106");
   }
+
+  @Test
+  public void testIssue116() throws IOException {
+    testDocs("links.issue116");
+  }
 }
